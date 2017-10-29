6872e9b28bbfd6dd6914b01416b4dd6d489d5abb
freddygv <vallenilla.fr@gmail.com>
2017-10-29 11:00:22 +0000

    downloader update

--- client/src/main/java/pt/fcup/Downloader.java
+++ client/src/main/java/pt/fcup/Downloader.java
@@ -1,85 +1,76 @@
 package pt.fcup;
 
-import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.io.*;
-import java.util.Properties;
 
 class Downloader extends Thread
 {
-	private String ip, hash, protocol, file;
-	private int port, chunkSize;
-	private byte[] contents;
-	private nbChunks;
+	private final String ip;
+	private final String protocol;
+	private final String file;
+	private final int port;
+	private final int chunkNumber;
 
-	public Downloader(String file, String ip, int port, String protocol, byte[] buf)
+	private int nbChunks;
+
+	public Downloader(String file, int chunkNumber, String ip, int port, String protocol)
 	{
 		super();
 		this.ip = ip;
-		this.hash = hash;
 		this.protocol = protocol;
 		this.port = port;
-		this.contents = buf;
 		this.file = file;
+		this.chunkNumber = chunkNumber;
 	}
 
 	@Override
 	public void run()
 	{
-		if(protocol != "TCP")
-		{
+		if (protocol != "TCP") {
 			System.out.println("Sorry, " + protocol + " is not yet supported!");
 			Thread.currentThread().interrupt();
 		}
 
-		try
-		{
-			System.out.println(System.getProperty("user.dir"));
-			//Initialize socket
-			Socket socket = new Socket(InetAddress.getByName(ip), port);
-			FileOutputStream fos = null;
+		try (
+				Socket clientSocket = new Socket(InetAddress.getByName(ip), port);
 
-			try
-			{
-				fos = new FileOutputStream("downloads/" + file);
-			}
-			catch(IOException e)
-	        	{
-	        		System.out.println("Couldn't create local file");
-	        		System.out.println("Please check if directory downloads exists");
-	        		System.out.println("    in same folder as jar");
-	           	e.printStackTrace();
-				Thread.currentThread().interrupt();
-	        	}
+				PrintWriter out =
+						new PrintWriter(clientSocket.getOutputStream(), true);
+				BufferedReader in =
+						new BufferedReader(
+								new InputStreamReader(clientSocket.getInputStream()));
 
-			DataInputStream dis = new DataInputStream(socket.getInputStream());
+				DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
 
-			// handshake
-			String propertiesText = dis.readUTF();
-			Properties properties = new Properties();
-			properties.load(new StringReader(propertiesText));
-			nbChunks = dis.readInt();
+				FileOutputStream fos = new FileOutputStream("downloads/" + file + "-" + chunkNumber);
+		) {
 
-			System.out.println("Number of chunks = " + nbChunks)
+			System.out.println("Running for chunk number " + chunkNumber);
+			out.println(chunkNumber);
 
-			//No of bytes read in one read() call
+
+			// TODO: Are these needed? where are these properties used
+//			// handshake
+//			String propertiesText = dis.readUTF();
+//			Properties properties = new Properties();
+//			properties.load(new StringReader(propertiesText));
+
+			nbChunks = Integer.parseInt(in.readLine());
+			System.out.println("Number of chunks is: " + nbChunks);
+
+			byte[] contents = new byte[1024*1024];
 			int bytesRead = 0;
+			while ((bytesRead = dis.read(contents)) > 0) {
+				fos.write(contents, 0, bytesRead);
 
-			while((bytesRead=dis.read(contents)) > 0)
-			{
-			    fos.write(contents, 0, bytesRead);
-			    // TODO add more relevant info
-			    System.out.println("Downloading chunk...");
 			}
 
 			fos.flush();
-			fos.close();
-			socket.close();
+			System.out.println(String.format("Downloaded chunk #%s of '%s' successfully", chunkNumber, file));
 
-			System.out.println("Downloaded chunk " + hash + " successfully");
+
 		}
 		catch(Exception e)
         {
