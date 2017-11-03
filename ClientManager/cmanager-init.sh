28327aab419b030aa1a0afcb36c5feb9e8dc7501
freddygv <vallenilla.fr@gmail.com>
2017-11-03 20:37:41 +0000

    removed old directories

--- ClientManager/cmanager-init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./wait-for-it.sh postgres-server:5432 --timeout=240
-./wait-for-it.sh seedbox:8082 --timeout=240
-mvn -U clean install exec:java
--- ClientManager/src/main/java/pt/fcup/Requestable.ice
+++
@@ -1,6 +0,0 @@
-module generated {
-    interface RequestableI {
-        int requestSeeder(string fileName);
-        bool disconnectClient();
-    };
-};
